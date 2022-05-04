843b98fbdd6a84b6b78d59f35a9b65f9880a2790
Ullavs <ullavansprang@gmail.com>
2022-05-04 23:04:58 +0200

    Oefening 1

--- projects/LOI/Oefening-1/script.js
+++
@@ -1,9 +0,0 @@
-window.alert("Pagina geladen");
-let myName = prompt("Please enter your name");
-let check = confirm(`Uw naam is ${myName}, is dit correct?`);
-
-if (check) {
-  document.body.innerHTML = `Welkom ${myName}`;
-} else {
-  prompt("Please enter your name");
-}
